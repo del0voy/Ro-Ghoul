ba0cdbf7779a4d7fafdb85bfc50fc90ea1f905ba
del0voy <150828079+del0voy@users.noreply.github.com>
2024-01-27 19:33:51 +0200

    Update roghoul.lua

--- roghoul.lua
+++ roghoul.lua
@@ -160,11 +160,11 @@ end, {min = 90, max = 250}):Set(45)
 
 tab2:AddSlider("Distance from NPC", function(x)
     myData.DistanceFromNpc = x * -1
-end, {min = 0, max = 8}):Set(65)
+end, {min = 0, max = 8}):Set(70)
 
 tab2:AddSlider("Distance from Bosses", function(x)
     myData.DistanceFromBoss = x * -1
-end, {min = 0, max = 15}):Set(55)
+end, {min = 0, max = 15}):Set(70)
 
 labels.p = {label = tab3:AddLabel("Текущий тренер: "..player.PlayerFolder.Trainers[team.."Trainer"].Value)}
 
