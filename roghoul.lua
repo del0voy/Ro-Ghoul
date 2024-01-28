8939921887d06559921e33704c88054e53238313
del0voy <150828079+del0voy@users.noreply.github.com>
2024-01-28 14:56:47 +0200

    Update roghoul.lua

--- roghoul.lua
+++ roghoul.lua
@@ -504,7 +504,7 @@ while true do
                                         pressKey(x)
                                     end
                                 end
-                                player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame * CFrame.Angles(math.rad(-180),0,0) + Vector3.new(0,30,0)
+                                player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0) + Vector3.new(math.rad(0),30,0)
                             else
                                 player.Character.HumanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame + npc.HumanoidRootPart.CFrame.lookVector * myData.DistanceFromNpc 
                             end
